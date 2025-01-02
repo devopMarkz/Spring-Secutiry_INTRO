312884df5013b41f0e3360736f441bf196484be7
devopMarkz <marcosdev2002@gmail.com>
2025-01-02 10:59:41 -0300

    Criando service e controller para autor

---
+++ src/main/java/io/github/marcos/libraryapi/controllers/AutorController.java
@@ -0,0 +1,12 @@
+package io.github.marcos.libraryapi.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/autores")
+public class AutorController {
+
+
+
+}
--- src/main/java/io/github/marcos/libraryapi/services/AutorService.java
+++ src/main/java/io/github/marcos/libraryapi/services/AutorService.java
@@ -1,4 +1,15 @@
 package io.github.marcos.libraryapi.services;
 
+import io.github.marcos.libraryapi.repositories.AutorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class AutorService {
+
+    @Autowired
+    private AutorRepository autorRepository;
+
+    //public
+
 }
