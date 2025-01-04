9e18764bbf9353d3e7e056a6d444ff577afd5fbc
devopMarkz <marcosdev2002@gmail.com>
2025-01-04 17:34:58 -0300

    Excluindo consultas desnecessarias apos uso do Query By Example

--- src/main/java/io/github/marcos/libraryapi/repositories/AutorRepository.java
+++ src/main/java/io/github/marcos/libraryapi/repositories/AutorRepository.java
@@ -2,22 +2,11 @@ package io.github.marcos.libraryapi.repositories;
 
 import io.github.marcos.libraryapi.model.Autor;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDate;
-import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
 public interface AutorRepository extends JpaRepository<Autor, UUID> {
-
-    List<Autor> findByNome(String nome);
-
-    List<Autor> findByNacionalidade(String nacionalidade);
-
-    @Query("SELECT obj FROM Autor obj WHERE UPPER(obj.nome) = UPPER(:nome) AND UPPER(obj.nacionalidade) = UPPER(:nacionalidade)")
-    List<Autor> findByNomeAndNacionalidade(@Param("nome") String nome, @Param("nacionalidade") String nacionalidade);
 
     Boolean existsByNomeAndDataNascimentoAndNacionalidade(String nome, LocalDate dataNascimento, String nacionalidade);
 
