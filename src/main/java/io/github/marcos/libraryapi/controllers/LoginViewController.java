09156c2ac09e7eb4355856efbe36f56ff2c37e66
devopMarkz <marcosdev2002@gmail.com>
2025-01-08 08:59:45 -0300

    Criando Controller para permitir acesso a tela de login customizada no Thymeleaf

--- src/main/java/io/github/marcos/libraryapi/controllers/LoginViewController.java
+++ src/main/java/io/github/marcos/libraryapi/controllers/LoginViewController.java
@@ -1,4 +1,13 @@
 package io.github.marcos.libraryapi.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class LoginViewController {
+
+    @GetMapping("/login")
+    public String paginaLogin(){
+        return "login";
+    }
 }
