74cbeaae0abf9d8669f2ddbe2b9104e543d19208
devopMarkz <marcosdev2002@gmail.com>
2025-01-08 21:12:42 -0300

    Configurando filtros de autenticacao por role de usuario

--- src/main/java/io/github/marcos/libraryapi/config/SecurityConfiguration.java
+++ src/main/java/io/github/marcos/libraryapi/config/SecurityConfiguration.java
@@ -6,6 +6,12 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 // Classe criada para efetuar configurações de Segurança
@@ -20,8 +26,36 @@ public class SecurityConfiguration {
                     configuration.loginPage("/login").permitAll();
                 })
                 .httpBasic(Customizer.withDefaults())
-                .authorizeHttpRequests(auth-> auth.anyRequest().authenticated())
+                .authorizeHttpRequests(auth-> {
+                    auth.requestMatchers("/login").permitAll();
+                    auth.requestMatchers("/autores/**").hasRole("ADMIN");
+                    auth.requestMatchers("/livros/**").hasAnyRole("USER", "ADMIN");
+                    auth.anyRequest().authenticated();
+                })
                 .build();
     }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder(10);
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder){
+        UserDetails user1 = User.builder()
+                .username("usuario")
+                .password(passwordEncoder().encode("123"))
+                .roles("USER")
+                .build();
+
+        UserDetails user2 = User.builder()
+                .username("admin")
+                .password(passwordEncoder().encode("321"))
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(user1, user2);
+    }
+
 
 }
