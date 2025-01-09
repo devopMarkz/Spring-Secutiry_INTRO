e113082887efe04383b9d22332a974606b50abd3
devopMarkz <marcosdev2002@gmail.com>
2025-01-09 08:49:29 -0300

    Criando entidade Usuario

---
+++ src/main/java/io/github/marcos/libraryapi/model/Usuario.java
@@ -0,0 +1,78 @@
+package io.github.marcos.libraryapi.model;
+
+import io.hypersistence.utils.hibernate.type.array.ListArrayType;
+import jakarta.persistence.*;
+import org.hibernate.annotations.Type;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.UUID;
+
+@Entity
+@Table(name = "usuario")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "login", unique = true)
+    private String login;
+
+    @Column(name = "senha")
+    private String senha;
+
+    @Type(ListArrayType.class)
+    @Column(name = "roles", columnDefinition = "varchar[]")
+    private List<String> roles;
+
+    public Usuario() {
+    }
+
+    public Usuario(UUID id, String login, String senha) {
+        this.id = id;
+        this.login = login;
+        this.senha = senha;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public List<String> getRoles() {
+        return roles;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null || getClass() != object.getClass()) return false;
+        Usuario usuario = (Usuario) object;
+        return Objects.equals(id, usuario.id) && Objects.equals(login, usuario.login) && Objects.equals(senha, usuario.senha);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, login, senha);
+    }
+}
