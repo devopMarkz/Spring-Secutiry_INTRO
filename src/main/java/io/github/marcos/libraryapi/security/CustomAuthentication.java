a41b6052c7cccdc81f0aaf3276e32e9702389180
devopMarkz <marcosdev2002@gmail.com>
2025-01-13 19:51:19 -0300

    Criando minha propria Authentication

---
+++ src/main/java/io/github/marcos/libraryapi/security/CustomAuthentication.java
@@ -0,0 +1,56 @@
+package io.github.marcos.libraryapi.security;
+
+import io.github.marcos.libraryapi.model.Usuario;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.Collection;
+
+public class CustomAuthentication implements Authentication {
+
+    private final Usuario usuario;
+
+    public CustomAuthentication(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return usuario.getRoles().stream().map(SimpleGrantedAuthority::new).toList();
+    }
+
+    @Override
+    public Object getCredentials() {
+        return null;
+    }
+
+    @Override
+    public Object getDetails() {
+        return usuario;
+    }
+
+    @Override
+    public Object getPrincipal() {
+        return usuario;
+    }
+
+    @Override
+    public boolean isAuthenticated() {
+        return true;
+    }
+
+    @Override
+    public void setAuthenticated(boolean isAuthenticated) throws IllegalArgumentException {
+
+    }
+
+    @Override
+    public String getName() {
+        return usuario.getLogin();
+    }
+}
