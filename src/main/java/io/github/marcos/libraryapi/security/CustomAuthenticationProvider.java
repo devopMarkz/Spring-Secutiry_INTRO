8d38b8d114d90a4cfd71e1f9d92ea8986e4b42a7
devopMarkz <marcosdev2002@gmail.com>
2025-01-13 21:20:49 -0300

    Criando um Authentication Provider customizado

---
+++ src/main/java/io/github/marcos/libraryapi/security/CustomAuthenticationProvider.java
@@ -0,0 +1,43 @@
+package io.github.marcos.libraryapi.security;
+
+import io.github.marcos.libraryapi.model.Usuario;
+import io.github.marcos.libraryapi.services.UsuarioService;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomAuthenticationProvider implements AuthenticationProvider {
+
+    private final UsuarioService usuarioService;
+    private final PasswordEncoder passwordEncoder;
+
+    public CustomAuthenticationProvider(UsuarioService usuarioService, PasswordEncoder passwordEncoder) {
+        this.usuarioService = usuarioService;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        String username = authentication.getName();
+        String password = authentication.getCredentials().toString();
+
+        Usuario usuario = usuarioService.obterPorLogin(username);
+
+        if(usuario == null) throw new UsernameNotFoundException("Usuário e/ou senha incorretos.");
+
+        if(passwordEncoder.matches(password, usuario.getSenha())){
+            return new CustomAuthentication(usuario);
+        }
+
+        throw new UsernameNotFoundException("Usuário e/ou senha incorretos.");
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return CustomAuthenticationProvider.class.isAssignableFrom(authentication);
+    }
+}
