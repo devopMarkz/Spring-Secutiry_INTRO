81bef675b793c34b186535e95e1f702cc721e8ef
devopMarkz <marcosdev2002@gmail.com>
2025-01-16 22:28:38 -0300

    Configurando token e client

--- src/main/java/io/github/marcos/libraryapi/security/CustomRegisteredClientRepository.java
+++ src/main/java/io/github/marcos/libraryapi/security/CustomRegisteredClientRepository.java
@@ -1,18 +1,26 @@
 package io.github.marcos.libraryapi.security;
 
 import io.github.marcos.libraryapi.services.ClientService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.oauth2.core.AuthorizationGrantType;
 import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
+import org.springframework.security.oauth2.server.authorization.settings.TokenSettings;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CustomRegisteredClientRepository implements RegisteredClientRepository {
 
-    @Autowired
     private ClientService clientService;
+    private TokenSettings tokenSettings;
+    private ClientSettings clientSettings;
+
+    public CustomRegisteredClientRepository(ClientService clientService, TokenSettings tokenSettings, ClientSettings clientSettings) {
+        this.clientService = clientService;
+        this.tokenSettings = tokenSettings;
+        this.clientSettings = clientSettings;
+    }
 
     @Override
     public void save(RegisteredClient registeredClient) {}
@@ -37,6 +45,8 @@ public class CustomRegisteredClientRepository implements RegisteredClientReposit
                 .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
                 .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
                 .authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
+                .tokenSettings(tokenSettings)
+                .clientSettings(clientSettings)
                 .build();
     }
 }
