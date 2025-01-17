610e561403fda0ded1da82f2b41c37435eca3ffd
devopMarkz <marcosdev2002@gmail.com>
2025-01-16 22:20:09 -0300

    Criando CustomRegisteredClientRepository para implementar RegisteredCLientRepository

---
+++ src/main/java/io/github/marcos/libraryapi/security/CustomRegisteredClientRepository.java
@@ -0,0 +1,42 @@
+package io.github.marcos.libraryapi.security;
+
+import io.github.marcos.libraryapi.services.ClientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomRegisteredClientRepository implements RegisteredClientRepository {
+
+    @Autowired
+    private ClientService clientService;
+
+    @Override
+    public void save(RegisteredClient registeredClient) {}
+
+    @Override
+    public RegisteredClient findById(String id) {
+        return null;
+    }
+
+    @Override
+    public RegisteredClient findByClientId(String clientId) {
+        var client = clientService.obterPorClientId(clientId);
+
+        if(client == null) return null;
+
+        return RegisteredClient
+                .withId(client.getId().toString())
+                .clientId(client.getClientId())
+                .clientSecret(client.getClientSecret())
+                .redirectUri(client.getRedirectURI())
+                .scope(client.getScope())
+                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+                .authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
+                .build();
+    }
+}
