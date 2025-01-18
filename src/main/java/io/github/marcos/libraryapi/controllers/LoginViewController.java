90667b29a1d38656ae78f0fa8598d2a49a1c1d77
devopMarkz <marcosdev2002@gmail.com>
2025-01-18 20:55:52 -0300

    Teste para capturar authorization code emitido pelo Authentication Server

--- src/main/java/io/github/marcos/libraryapi/controllers/LoginViewController.java
+++ src/main/java/io/github/marcos/libraryapi/controllers/LoginViewController.java
@@ -3,6 +3,7 @@ package io.github.marcos.libraryapi.controllers;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -17,5 +18,11 @@ public class LoginViewController {
     @ResponseBody
     public String paginaHome(Authentication authentication){
         return "olá " + authentication.getName();
+    }
+
+    @GetMapping("/authorized")
+    @ResponseBody
+    public String getAuthCode(@RequestParam("code") String code){
+        return "Seu authorization code " + code;
     }
 }
