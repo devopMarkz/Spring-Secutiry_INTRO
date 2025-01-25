84219c6f8ce76a91b0bd6d1cf6ca6a423176fbb1
devopMarkz <marcosdev2002@gmail.com>
2025-01-25 17:32:09 -0300

    Personalizando DTO e seus atributos na documentacao

--- src/main/java/io/github/marcos/libraryapi/dto/autor/CreateAutorDTO.java
+++ src/main/java/io/github/marcos/libraryapi/dto/autor/CreateAutorDTO.java
@@ -1,18 +1,26 @@
 package io.github.marcos.libraryapi.dto.autor;
 
 import io.github.marcos.libraryapi.model.Autor;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 import java.time.LocalDate;
 
+@Schema(name = "Autor")
 public record CreateAutorDTO(
+        @Schema(name = "nome")
         @NotBlank(message = "Campo obrigatório.")
         String nome,
+
+        @Schema(name = "dataNascimento")
         @NotNull(message = "Campo obrigatório.")
         LocalDate dataNascimento,
+
+        @Schema(name = "nacionalidade")
         @NotBlank(message = "Campo obrigatório.")
-        String nacionalidade) {
+        String nacionalidade
+) {
 
     public Autor mapearParaAutor(){
         Autor autor = new Autor();
