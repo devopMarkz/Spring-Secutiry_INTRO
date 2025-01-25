648977ab048b4b238ee1bdd986828f0a7bda256e
devopMarkz <marcosdev2002@gmail.com>
2025-01-25 17:22:50 -0300

    Documentando controller

--- src/main/java/io/github/marcos/libraryapi/controllers/AutorController.java
+++ src/main/java/io/github/marcos/libraryapi/controllers/AutorController.java
@@ -4,19 +4,22 @@ import io.github.marcos.libraryapi.dto.autor.AutorResponseDTO;
 import io.github.marcos.libraryapi.dto.autor.CreateAutorDTO;
 import io.github.marcos.libraryapi.dto.autor.UpdateAutorDTO;
 import io.github.marcos.libraryapi.services.AutorService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
 import java.util.List;
 
 @RestController
 @RequestMapping("/autores")
+@Tag(name = "Autores")
 public class AutorController implements GenericController {
 
     @Autowired
@@ -24,6 +27,12 @@ public class AutorController implements GenericController {
 
     @PostMapping
     @PreAuthorize("hasRole('GERENTE')")
+    @Operation(summary = "Salvar", description = "Cadastrar novo autor")
+    @ApiResponses({
+            @ApiResponse(responseCode = "201", description = "Cadastrado com sucesso."),
+            @ApiResponse(responseCode = "422", description = "Erro de validação."),
+            @ApiResponse(responseCode = "409", description = "Autor já cadastrado")
+    })
     public ResponseEntity<Void> salvar(@Valid @RequestBody CreateAutorDTO createAutorDTO){
         AutorResponseDTO autorResponseDTO = autorService.insert(createAutorDTO);
         URI location = gerarHeaderLocation(autorResponseDTO.id());
