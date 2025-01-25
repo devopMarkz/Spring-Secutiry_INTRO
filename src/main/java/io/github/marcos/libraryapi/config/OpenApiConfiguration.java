aff451936db2e7541bbc26c593727f7ff9d6c91d
devopMarkz <marcosdev2002@gmail.com>
2025-01-25 19:45:48 -0300

    Adicionando Autenticacao ao Swagger Open API

--- src/main/java/io/github/marcos/libraryapi/config/OpenApiConfiguration.java
+++ src/main/java/io/github/marcos/libraryapi/config/OpenApiConfiguration.java
@@ -1,8 +1,12 @@
 package io.github.marcos.libraryapi.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -13,7 +17,17 @@ import org.springframework.context.annotation.Configuration;
                 contact = @Contact(name = "Marcos André",
                                     email = "marcosdev2002@gmail.com"
                 )
-        )
+        ),
+        security = {
+                @SecurityRequirement(name = "bearerAuth")
+        }
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        scheme = "bearer",
+        in = SecuritySchemeIn.HEADER
 )
 public class OpenApiConfiguration {
 }
